9e2a9edf954251d4752d103230b842c7a5a54780
Enrique Ruvalcaba <jordison352@gmail.com>
2022-12-06 13:35:37 -0600

    Component completed. js and readme added.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Interactive rating component
+
+Practicing web design and Vanilla Javascript
+
+[See the website](https://cthulhuscode.github.io/interactive-rating-component)
+
+<br>
+
+[Figma design](https://www.figma.com/file/yp2tWIPhdf6igLrRJMwEBh/interactive-rating-component)
--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const ratings = document.querySelector(".card-1__rating");
+const optionSelectedClass = "rating__option--selected";
+const ratingOptions = [];
+let value;
+
+/* Add options for rating control */
+for (let i = 0; i < 5; i++) {
+  const ratingOption = document.createElement("div");
+  const optionNum = document.createTextNode(i + 1);
+
+  ratingOption.setAttribute("value", i + 1);
+  ratingOption.appendChild(optionNum);
+  ratingOption.classList.add("rating__option");
+  ratingOption.addEventListener("click", handleOptionClick);
+
+  ratings.appendChild(ratingOption);
+}
+
+/* Handle submit */
+const submitBtn = document.querySelector(".card-1__button");
+submitBtn.addEventListener("click", handleSubmit);
+
+function handleSubmit(e) {
+  if (!value) return;
+
+  const card1 = document.querySelector(".card-1");
+  card1.style.cssText = "display: none;";
+
+  const card2 = document.querySelector(".card-2");
+  card2.style.cssText = "display: flex";
+
+  const ratingFeedback = document.querySelector(".card-2__secondary-text");
+  const feedback = document.createTextNode(`You selected ${value} out of 5`);
+  ratingFeedback.innerHTML = "";
+  ratingFeedback.appendChild(feedback);
+}
+
+function handleOptionClick(e) {
+  const option = e.target;
+  value = option.getAttribute("value");
+  removeSelectedOption();
+
+  option.classList.add(optionSelectedClass);
+}
+
+function removeSelectedOption() {
+  const ratingOptions = document.querySelector(".card-1__rating").children;
+
+  for (let option of ratingOptions) {
+    option.classList.remove(optionSelectedClass);
+  }
+}
